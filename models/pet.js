9161ab28aa159974c98987ffb9d8ed8f079b5125
jesse <jessemspringer@gmail.com>
2018-04-06 00:58:45 -0400

    created Sequelize modal for Pets table - next need to associate with Owner table

--- models/pet.js
+++ models/pet.js
@@ -1,5 +1,33 @@
 module.exports = function(sequelize, DataTypes) {
     var Pet = sequelize.define("Pet",  {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        gender: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        size: {
+            type: DataTypes.STRING,
+            allowNull: false
+            
+        },
+        bio: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        breed: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    });
 
-    })
+    return Pet;
 }
+
+// next step: associate Pets table with Owner table
