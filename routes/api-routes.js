cc77c307d9e95753d888acd66b971773026126a4
George Greene <georgegreene@outlook.com>
2018-04-11 21:52:27 -0400

    pets now in MySQL

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,20 +4,34 @@ var db = require("../models")
 module.exports = function(app) {
 
 
-    app.post("/api/owner", function(req, res) {
-        console.log(req);
-     
-        db.Owner.create({
-          first_name: req.body.firstName,
-          last_name: req.body.lastName,
-          photo: req.body.photo,
-          age: req.body.age,
-          gender: req.body.gender,
-          location: req.body.location
-        }).then(function(dbOwner) {
-          
-          res.json(dbOwner);
-        });
-      });
+  app.post("/api/owner", function(req, res) {
+    console.log(req);
+ 
+    db.Owner.create({
+      first_name: req.body.firstName,
+      last_name: req.body.lastName,
+      photo: req.body.photo,
+      age: req.body.age,
+      gender: req.body.gender,
+      location: req.body.location
+    }).then(function(dbOwner) {
+      res.json(dbOwner);
+    });
+  });
+
+  app.post("/api/pet", function(req, res) {
+    console.log(req);
+
+    db.Pet.create({
+      name: req.body.name,
+      age: req.body.scores[1],
+      gender: req.body.scores[2],
+      size: req.body.scores[3],
+      breed: req.body.scores[4],
+      bio: req.body.scores[5]
+    }).then(function(dbPet) {
+      res.json(dbPet);
+    });
+  });
 
 }
