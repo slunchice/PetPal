a4c1f0e2bb8a54fe7f6720911710a1341785a898
George Greene <georgegreene@outlook.com>
2018-04-14 09:38:30 -0400

    a

--- public/js/eventBrite.js
+++ public/js/eventBrite.js
@@ -1,3 +1,16 @@
 $.get("/api/events", function(data) {
   console.log(data);
+  var newEvent = {
+    logo: event.logo.url,
+    name: event.name.html,
+    time: event.start.local,
+    url: event.url
+  }
+  // Appends each newEvent object to respective DOM elements
+  // $("#eventLogo" + i + "").html("<img style='border:10px solid black;padding:5px;'id='logo' src='" + newEvent.logo + "'/>");
+  // $("#eventName" + i + "").html(newEvent.name);
+  // $("#eventTime" + i + "").html(newEvent.time);
+  // var string = "Click here for more details!";
+  // var result = string.link("" + newEvent.url + "");
+  // $("#eventUrl" + i + "").html(result);
 });
