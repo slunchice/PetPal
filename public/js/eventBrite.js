4e5bbc8d86abd4c68ae3d3d0db5286c8abe8647e
George Greene <georgegreene@outlook.com>
2018-04-14 09:46:26 -0400

    change

--- public/js/eventBrite.js
+++ public/js/eventBrite.js
@@ -1,16 +1,23 @@
 $.get("/api/events", function(data) {
-  console.log(data);
-  var newEvent = {
-    logo: event.logo.url,
-    name: event.name.html,
-    time: event.start.local,
-    url: event.url
+  // console.log(data);
+  if (data.length) {
+    for (var i = 0; i < 3; i++) {
+      var event = data[i];
+      // Creates newEvent object with each return object in for loop
+      var newEvent = {
+        logo: event.logo.original.url,
+        name: event.name.html,
+        description: event.description.html,
+        link: event.url
+      }
+      console.log(newEvent);
+      // Appends each newEvent object to respective DOM elements
+      $("#eventLogo" + i + "").html("<img style='border:10px solid black;padding:5px;'id='logo' src='" + newEvent.logo + "'/>");
+      $("#eventName" + i + "").html(newEvent.name);
+      $("#eventDescription" + i + "").html(newEvent.description);
+      var string = "See more";
+      var result = string.link("" + newEvent.url + "");
+      $("#eventLink" + i + "").html(result);
+    }
   }
-  // Appends each newEvent object to respective DOM elements
-  // $("#eventLogo" + i + "").html("<img style='border:10px solid black;padding:5px;'id='logo' src='" + newEvent.logo + "'/>");
-  // $("#eventName" + i + "").html(newEvent.name);
-  // $("#eventTime" + i + "").html(newEvent.time);
-  // var string = "Click here for more details!";
-  // var result = string.link("" + newEvent.url + "");
-  // $("#eventUrl" + i + "").html(result);
 });
